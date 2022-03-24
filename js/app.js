d38d2743046949388f79011182264bd535b16c66
Sbeltran16 <sbeltran16@gmail.com>
2022-03-24 01:18:30 -0700

    Changes to css

--- js/app.js
+++ js/app.js
@@ -1,21 +1,14 @@
-// We are going to create the dialoogue and options for the game
-//First we have to make a varible that holds all of our options and text.
-// We will make it with ojects that holds other objects and arrays   
+  
 let story = {
-    // This is the where the first scene will start at.
     currentPlace: 'outside',
     outside: {
-        // We hav our title with a key named sceneTitle.
+        img: "Horror House outside.jpeg",
         title: "Outside",
-        //This is where our story text is going to go.
         text: `Its a cold day on a Halloween night. You are walking
         down the neighborhood and you see an old white house with a
         old tree in the front yard and tall dead grass. You approch
         the house and hear a loud scream come from the inside.
         You get curious and enter the house. What do you do??`, 
-        //We will have our options and they are going to be in an array.
-        // Our array will have two options and the will hold the choice of the player.
-        //and the next location that the choice takes you to.
         options: [
            {
             choice: "Go to the living room",
@@ -28,8 +21,8 @@ let story = {
            }
        ]
    },
-   // Here we are making more scenes excatly how we did above.
     livingRoom: {
+        img: "livingRoom.jpeg",
         title: "Living Room",
         text: `You enter the house and see a long hallway. It is dark and cold as you continue to walk down the hall and
         end up in the living room. You can not believe what you see!! There
@@ -50,6 +43,7 @@ let story = {
         ] 
    },
    kitchen: {
+       img: "kitchen.png",
        title: "Kitchen",
        text: `You attempt to talk to the ghost. To your suprise she actually responds.
        Her voice sounds rough and she explains what has happened over the years
@@ -73,6 +67,7 @@ let story = {
        ]
    },
    winGame: {
+       img: "bedroom.jpeg",
        title: "Bedroom (Good Ending)",
        text: `You go into the bedroom and you discover that there is a window that is wide open.
         it leads to the backyard, where you can escape. You decide to take your chances and make it out alive!!
@@ -86,6 +81,7 @@ let story = {
         
    },
    gameOver1: {
+       img: "basment.jpeg",
        title: "Unfortunate Circumstances! (Alternate Ending)",
        text: `You decide to run as fast as you can but do not realise a hatch on the floor. You fall right through it and break both legs.
        There is a demogorgan and he has been waiting for someone to show up. You try to fight it but it ends up eating you whole. You died....`,
@@ -98,6 +94,7 @@ let story = {
    },
 
    gameOver2: {
+       img: "basement.jpeg",
        title: "Basement (Bad Ending)",
        text: `You decided to go into the basement. It is dark but you find the light switch. You turn on the lights and and find a demongorgon.
        He had been waiting for someone show up. You try to fight it but it ends up eating you whole. You died....`,
@@ -119,49 +116,49 @@ let story = {
 
 
 // Where we are going to keep some our query selectors
-let start = document.querySelector('#start')
-let content = document.querySelector("#container")
+let start = document.querySelector('#start');
+let content = document.querySelector("#container");
     // I add an event listener for the start button that allow
     // us to click it and render the scene that we want it to go to.
-    start.addEventListener('click', renderScene)
+    start.addEventListener('click', renderScene);
 
 // I made a function for my html content that will change.
 // Inside is a I have content.innerHTMl which will update the html.
 function renderScene() {
+    let img = ""
+    if (story[story.currentPlace].img){
+        img = '<img></img>'
+    }
     //Here is where I will update the html with innerHTML.
     // I also create a new buttton named continue that will stay with all the scenes.
     content.innerHTML = `
+    
     <h1 id="gameHeader">${story[story.currentPlace].title}</h1>
+    ${img}
     <p>${story[story.currentPlace].text}</p>
     ${getPlayerInput()}
     <button id ="continue">Continue</button>
     `
+    if (story[story.currentPlace].img){
+        document.querySelector('img').src = `./imgs/${story[story.currentPlace].img}`
+    }
     // I pull the new continue button with a query selector and make a new
     //event listener for it that will take me to the new scene when the player input is chosen.
     let continueButton = document.querySelector("#continue");
     continueButton.addEventListener('click', function() {
-        // below will give me the input value of the choice that the player chooses.
-        //This will trigger the scene to change.
         getPlayerInputValue()
     })
 }
 
-// Another function is made that checks for the input value of the players input.
 function getPlayerInputValue() {
-    // Here we grab all of the input types of radio with query selector all.
     let playerInput = document.querySelectorAll('input[type="radio"]');
-    // I make a for loop to that will continue to loop with the amount of choices
-    // that there is. 
     for (let i = 0; i < playerInput.length; i++){
-        // I make an if loop that checks (with .checked) if the [i] is true.
-        // if they both are true then it will return renderScene().
         if(playerInput[i].checked){
             story.currentPlace = playerInput[i].getAttribute("data-next")
             renderScene();
         }
     }
 }
-
 
 function getPlayerInput() {
     let input = ""
